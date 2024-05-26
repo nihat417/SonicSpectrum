fc51b731a9a04009ab5a1189e6ffa0518fff4645
nihat417 <akreminihat@gmail.com>
2024-05-26 20:13:21 +0400

    Version 4.7.0 Added DI

--- SonicSpectrum.Presentation/Program.cs
+++ SonicSpectrum.Presentation/Program.cs
@@ -1,106 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SonicSpectrum.Persistence.Data;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
-using SonicSpectrum.Domain.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Identity;
-using SonicSpectrum.Application.Repository.Abstract;
-using SonicSpectrum.Application.Repository.Concrete;
-using IdentityManagerServerApi.Services;
-using SonicSpectrum.Application.Models;
-using System.Text.Json.Serialization;
+﻿using SonicSpectrum.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCustomServices(builder.Configuration);
+builder.Services.AddCustomAuthentication(builder.Configuration);
+builder.Services.AddCustomIdentity();
+builder.Services.AddCustomCors();
+builder.Services.AddCustomDbContext(builder.Configuration);
+builder.Services.AddCustomSwagger();
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:3000")
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("default") ??
-        throw new InvalidOperationException("Connection String is not found"));
-});
-
-builder.Services.AddIdentity<User, IdentityRole>(option =>
-{
-    option.SignIn.RequireConfirmedEmail = true;
-})
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddSignInManager()
-    .AddRoles<IdentityRole>();
-
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-});
-
-
-// JWT 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateIssuerSigningKey = true,
-        ValidateLifetime = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-    };
-});
-
-
-builder.Services.AddSwaggerGen( options =>
-{
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-    });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-});
-
-var emailConfig = builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
-
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
-
-builder.Services.AddSingleton(emailConfig!);
-builder.Services.AddSingleton<JwtTokenService>();
-builder.Services.AddScoped<IAuthService,AuthService>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddScoped<IMusicSettingService,MusicSettingService>();
-
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
