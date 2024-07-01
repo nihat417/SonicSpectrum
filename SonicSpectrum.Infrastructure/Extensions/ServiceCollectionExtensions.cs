d87144bad84fbb6e7ea6e88a6f66be632b27e015
nihat417 <akreminihat@gmail.com>
2024-07-01 23:41:31 +0400

    version 5.0.2 Publish Azure

--- SonicSpectrum.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ SonicSpectrum.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -91,7 +91,7 @@ namespace SonicSpectrum.Infrastructure.Extensions
         {
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseSqlServer(configuration.GetConnectionString("default") ??
+                options.UseSqlServer(configuration.GetConnectionString("azureDef") ??
                     throw new InvalidOperationException("Connection String is not found"));
             });
 
