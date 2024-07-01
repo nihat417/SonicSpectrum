301b6b3a432a2a6c0adbdab47af0e511e0235682
nihat417 <akreminihat@gmail.com>
2024-07-02 00:06:41 +0400

    version 5.0.3 fix bug

--- SonicSpectrum.Presentation/Program.cs
+++ SonicSpectrum.Presentation/Program.cs
@@ -1,31 +1,15 @@
 ﻿using SonicSpectrum.Infrastructure.Extensions;
+using SonicSpectrum.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCustomServices(builder.Configuration);
-builder.Services.AddCustomAuthentication(builder.Configuration);
-builder.Services.AddCustomIdentity();
-builder.Services.AddCustomCors();
-builder.Services.AddCustomDbContext(builder.Configuration);
-builder.Services.AddCustomSwagger();
 
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("AllowSpecificOrigin");
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ SonicSpectrum.Presentation/Startup.cs
@@ -0,0 +1,47 @@
+﻿using SonicSpectrum.Infrastructure.Extensions;
+
+namespace SonicSpectrum.Presentation
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCustomServices(_configuration);
+            services.AddCustomAuthentication(_configuration);
+            services.AddCustomIdentity();
+            services.AddCustomCors();
+            services.AddCustomDbContext(_configuration);
+            services.AddCustomSwagger();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowSpecificOrigin");
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
