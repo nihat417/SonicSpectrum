b804cd27f1fce06bc30a1baafd675cadbcb41f38
nihat417 <akreminihat@gmail.com>
2024-08-13 15:00:43 +0400

    Update 1.2

---
+++ SonicSpectrum.Application/DTOs/UserInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using SonicSpectrum.Domain.Entities;
+
+namespace SonicSpectrum.Application.DTOs
+{
+    public class UserInfoDto
+    {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? FullName { get; set; }
+        public int Age { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool IsProfileOpen { get; set; }
+        public ICollection<PlaylistDTO>? Playlists { get; set; }
+
+    }
+}
